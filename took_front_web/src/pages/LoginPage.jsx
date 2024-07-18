4939fb1f3877900ee8dfc31e0bdf120347a2c70e
정희수 <s007kk@ajou.ac.kr>
2024-07-18 12:20:55 +0900

    feat: 로그인 UI 수정

--- took_front_web/src/pages/LoginPage.jsx
+++ took_front_web/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import kakaoImage from "../assets/login/kakao.svg";
 import googleImage from "../assets/login/google.png";
+import backIcon from "../assets/common/back.svg";
 
 function LoginPage() {
   const [userName, setUserName] = useState('');
@@ -11,20 +12,27 @@ function LoginPage() {
     console.log("pwd:", password);
     // TODO: 로그인 API 호출
   }
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white p-4">
-      <div className="text-main font-dela  text-6xl mb-10">took !</div>
+  return ( 
+  <>
+  <img
+            src={backIcon}
+            alt="뒤로"
+            className="w-6 h-6 mr-2"
+          />
+    <div className="flex flex-col w-[70%] items-center justify-center min-h-screen bg-white p-4 mx-auto">
+
+      <div className="text-main font-dela  text-4xl mb-10">took !</div>
       <div className="w-full max-w-xs">
         <div className="mb-4">
           <input
-            className="appearance-none block w-full  placeholder-main text-main text-xl font-semibold placeholder:text-lg border-b-2 border-main rounded py-3 px-3 my-4 leading-tight focus:outline-none focus:bg-white focus:border-main"
+            className="appearance-none block w-full  placeholder-main text-main text-lg placeholder:text-sm placeholder:text-opacity-60 border-b-2 border-main rounded py-2 px-1 my-4 leading-tight focus:outline-none focus:bg-white focus:border-main"
             type="text"
             value={userName}
             onChange={(e) => setUserName(e.target.value)}
             placeholder="아이디"
           />
           <input
-            className="aappearance-none block w-full  placeholder-main text-main text-xl font-semibold placeholder:text-lg border-b-2 border-main rounded py-3 px-3 my-4 leading-tight focus:outline-none focus:bg-white focus:border-main"
+            className="appearance-none block w-full  placeholder-main text-main text-lg placeholder:text-sm placeholder:text-opacity-60 border-b-2 border-main rounded py-2 px-1 my-4 leading-tight focus:outline-none focus:bg-white focus:border-main"
             type="password"
             placeholder="비밀번호"
             value={password}
@@ -32,18 +40,18 @@ function LoginPage() {
           />
         </div>
         <button
-          className="w-full bg-main text-white font-extrabold text-xl py-3 mt-12 px-4 rounded-2xl shadow-md focus:outline-none focus:shadow-outline"
+          className="w-full bg-main text-white font-bold text-lg py-3 mt-8 px-4 rounded-2xl shadow-md focus:outline-none focus:shadow-outline"
           type="submit" onClick={handleLoginClick}
         >
           로그인
         </button>
-        <div className="flex justify-between text-main text-base mb-12 mt-6">
-          <a href="#" className="hover:underline">회원가입</a>
-          <a href="#" className="hover:underline">비밀번호 찾기</a>
+        <div className="flex justify-between text-main text-sm mb-12 mt-6">
+          <a href="/signup" className="hover:underline">회원가입</a>
+          <a href="/userinfo" className="hover:underline">비밀번호 찾기</a>
         </div>
-        <div className="flex items-center my-8">
+        <div className="flex items-center my-6">
           <hr className="flex-grow border-t border-main" />
-          <span className="mx-2 text-main">또는</span>
+          <span className="mx-2 text-main text-sm">또는</span>
           <hr className="flex-grow border-t border-main" />
         </div>
         <button
@@ -58,7 +66,7 @@ function LoginPage() {
           카카오로 로그인
         </button>
         <button
-          className="w-full bg-blue-500 text-white font-bold py-3  px-4 rounded-2xl shadow-md focus:outline-none focus:shadow-outline flex items-center justify-center"
+          className="w-full font-nanum bg-blue-500 font-bold text-white py-3  px-4 rounded-2xl shadow-md focus:outline-none focus:shadow-outline flex items-center justify-center"
           type="button"
         >
           <img
@@ -69,7 +77,7 @@ function LoginPage() {
           구글로 로그인
         </button>
       </div>
-    </div>
+    </div></>
   );
 }
 
--- took_front_web/tailwind.config.js
+++ took_front_web/tailwind.config.js
@@ -3,13 +3,16 @@ export default {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
-            colors:{
+            colors: {
                 main: '#FF7F50',
             },
             fontFamily: {
                 dela: ['"Dela Gothic One"', 'sans-serif'],
                 nanum: ['"Noto Sans KR"', 'sans-serif'],
             },
+        },
+        fontFamily: {
+            sans: ['"Nanum Gothic"', 'sans-serif'],
         },
     },
     plugins: [],
