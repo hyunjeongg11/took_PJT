64d04bf0c0950ae889546124d1ce6e787a6bb309
차민주 <ckalswn007@naver.com>
2024-07-23 17:44:39 +0900

    feat: 배달 took 게시물 작성 UI 구현 중

--- took_front_web/src/App.jsx
+++ took_front_web/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {MainPage, LoginPage, PaymentPage, SignupPage, LocationSettingPage, ModifyPwdPage, MyPage, NotificationSetting, TookDetailsPage, TookHistoryPage, UserInfoPage, AccountCompletePage, AccountPage, AgreementDetailPage, AgreementPage, VerificationPage, SelectPage, CompletePage, PwdPage, PaymentInputPage, UserListPage } from "./pages";
 import PaymentMethods from "./pages/oneclick/PaymentMethodsPage"
+import CreateDeliveryPage from "./pages/delivery/CreateDeliveryPage"
+
 
 
 
@@ -42,7 +44,8 @@ function App() {
     { path: "/complete", element: <CompletePage />},
     { path: "/paymentmethods", element: <PaymentMethods /> },
     { path: "/dutch/userlist", element: <UserListPage />},
-    { path: "/dutch/input", element: <PaymentInputPage />}
+    { path: "/dutch/input", element: <PaymentInputPage />},
+    { path: "/create/delivery", element: <CreateDeliveryPage />},
   ]);
 
   return <RouterProvider router={ROUTER} />;
