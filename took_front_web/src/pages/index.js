0380a0d02eb1a42a363f6125a8ba8a471eaf0ea7
정희수 <s007kk@ajou.ac.kr>
2024-07-23 10:57:03 +0900

    refactor: react-router 리팩토링

---
+++ took_front_web/src/pages/index.js
@@ -0,0 +1,8 @@
+export * from "./mypage";
+export * from "./AccountRegistration";
+export {default as LoginPage} from "./LoginPage";
+export {default as MainPage} from "./MainPage";
+export {default as SignupPage} from "./SignupPage";
+export {default as CompletePage} from "./temp/CompletePage";
+export {default as PaymentPage} from "./temp/PaymentPage";
+export {default as PwdPage} from "./temp/PwdPage";
