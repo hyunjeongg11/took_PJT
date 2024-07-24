3035e4709c6e56d92b6bb52f45ce7e71d6b9bcb6
조현정 <guswjd4585@gmail.com>
2024-07-25 02:08:07 +0900

    refactor : myTook 페이지의 보낼돈 화면 송금하기 버튼 수정

--- took_web/src/components/payment/SendMoneyCard.jsx
+++ took_web/src/components/payment/SendMoneyCard.jsx
@@ -5,22 +5,22 @@ import { formatDateWithoutTime } from '../../utils/formatDate';
 
 const SendMoneyCard = ({ imgNo, userName, cost, createdAt, status }) => {
   return (
-    <div className={`flex justify-between items-center p-5 mb-4 bg-white border border-slate-100 rounded-2xl shadow-lg ${status ? 'opacity-50' : ''}`}>
-      <div className="flex items-center">
-        <img src={getProfileImagePath(imgNo)} alt={userName} className="w-10 h-10 mr-4" />
-        <div>
-          <div className="text-sm font-bold">{userName}</div>
-          <div className="text-lg font-bold">{formatNumber(cost)}원</div>
+    <div className={`flex flex-col justify-between p-5 mb-4 bg-white border border-slate-100 rounded-2xl shadow-lg ${status ? 'opacity-50' : ''}`}>
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center">
+          <img src={getProfileImagePath(imgNo)} alt={userName} className="w-10 h-10 mr-4" />
+          <div>
+            <div className="text-sm font-bold">{userName}</div>
+            <div className="text-lg font-bold">{formatNumber(cost)}원</div>
+          </div>
         </div>
+        <div className="text-xs text-gray-500">{formatDateWithoutTime(createdAt)}</div>
       </div>
-      <div className="text-right flex flex-col justify-between items-end h-full">
-        <div className="text-xs text-gray-500 mb-2">{formatDateWithoutTime(createdAt)}</div>
-        {status ? (
-          <div className="text-xs text-gray-500">송금완료</div>
-        ) : (
-          <button className="text-xs text-white bg-main py-1 px-2 rounded mt-auto">송금하기</button>
-        )}
-      </div>
+      {status ? (
+        <div className="text-xs text-gray-500 text-right text-end">송금완료</div>
+      ) : (
+        <button className="text-s text-white bg-main py-2 px-12 rounded-2xl self-center">송금하기</button>
+      )}
     </div>
   );
 };
--- took_web/src/pages/oneclick/MyTookPage.jsx
+++ took_web/src/pages/oneclick/MyTookPage.jsx
@@ -23,8 +23,8 @@ const tempParties = [
     totalCost: 71600, 
     status: "정산완료", 
     createdAt: "2024-06-24T18:55:00", 
-    totalMembers: 4, 
-    settledMembers: 4,
+    totalMembers: 5, 
+    settledMembers: 5,
   },
   { 
     party_idx: 3, 
@@ -32,8 +32,8 @@ const tempParties = [
     totalCost: 120500, 
     status: "정산완료", 
     createdAt: "2024-06-22T20:11:00", 
-    totalMembers: 5, 
-    settledMembers: 5,
+    totalMembers: 6, 
+    settledMembers: 6,
   },
   { 
     party_idx: 4, 
@@ -41,26 +41,29 @@ const tempParties = [
     totalCost: 20500, 
     status: "정산완료", 
     createdAt: "2024-06-15T11:49:00", 
-    totalMembers: 3, 
-    settledMembers: 3,
+    totalMembers: 4, 
+    settledMembers: 4,
   }
 ];
 
 const tempMembers = [
   { member_seq: 1, party_seq: 1, user_seq: 1, userName: '조현정', imgNo: 1, cost: 6600, status: false, receive: false, isLeader: false, createdAt: "2024-07-06T01:49:00" },
   { member_seq: 2, party_seq: 1, user_seq: 2, userName: '김성훈', imgNo: 2, cost: 5400, status: false, receive: false, isLeader: false, createdAt: "2024-07-06T01:49:00" },
-  { member_seq: 3, party_seq: 1, user_seq: 3, userName: '이민찬', imgNo: 3, cost: 5400, status: true, receive: true, isLeader: true, createdAt: "2024-07-06T01:49:00" },
+  { member_seq: 3, party_seq: 1, user_seq: 3, userName: '이민찬', imgNo: 3, cost: 5400, status: false, receive: true, isLeader: true, createdAt: "2024-07-06T01:49:00" },
 
+  { member_seq: 1, party_seq: 2, user_seq: 1, userName: '조현정', imgNo: 1, cost: 6600, status: true, receive: false, isLeader: false, createdAt: "2024-07-06T01:49:00" },
   { member_seq: 4, party_seq: 2, user_seq: 4, userName: '공재환', imgNo: 4, cost: 18550, status: true, receive: true, isLeader: true, createdAt: "2024-06-24T18:55:00" },
   { member_seq: 5, party_seq: 2, user_seq: 5, userName: '차민주', imgNo: 5, cost: 20500, status: true, receive: true, isLeader: false, createdAt: "2024-06-24T18:55:00" },
   { member_seq: 6, party_seq: 2, user_seq: 6, userName: '조경현', imgNo: 6, cost: 6900, status: false, receive: false, isLeader: false, createdAt: "2024-06-24T18:55:00" },
   
+  { member_seq: 1, party_seq: 3, user_seq: 1, userName: '조현정', imgNo: 1, cost: 6600, status: true, receive: false, isLeader: false, createdAt: "2024-07-06T01:49:00" },
   { member_seq: 7, party_seq: 3, user_seq: 7, userName: '박수현', imgNo: 7, cost: 20500, status: true, receive: true, isLeader: true, createdAt: "2024-06-22T20:11:00" },
   { member_seq: 8, party_seq: 3, user_seq: 8, userName: '정수화', imgNo: 8, cost: 2500, status: true, receive: true, isLeader: false, createdAt: "2024-06-22T20:11:00" },
   { member_seq: 9, party_seq: 3, user_seq: 9, userName: '송수진', imgNo: 9, cost: 3500, status: true, receive: true, isLeader: false, createdAt: "2024-06-22T20:11:00" },
   { member_seq: 10, party_seq: 3, user_seq: 10, userName: '강선아', imgNo: 10, cost: 5500, status: false, receive: false, isLeader: false, createdAt: "2024-06-22T20:11:00" },
   { member_seq: 11, party_seq: 3, user_seq: 11, userName: '윤소경', imgNo: 11, cost: 1500, status: true, receive: true, isLeader: false, createdAt: "2024-06-22T20:11:00" },
 
+  { member_seq: 1, party_seq: 4, user_seq: 1, userName: '조현정', imgNo: 1, cost: 6600, status: true, receive: false, isLeader: false, createdAt: "2024-07-06T01:49:00" },
   { member_seq: 12, party_seq: 4, user_seq: 12, userName: '최민정', imgNo: 12, cost: 6000, status: true, receive: true, isLeader: false, createdAt: "2024-06-15T11:49:00" },
   { member_seq: 13, party_seq: 4, user_seq: 13, userName: '신동민', imgNo: 13, cost: 7000, status: true, receive: true, isLeader: true, createdAt: "2024-06-15T11:49:00" },
   { member_seq: 14, party_seq: 4, user_seq: 14, userName: '임지연', imgNo: 14, cost: 7500, status: true, receive: true, isLeader: false, createdAt: "2024-06-15T11:49:00" }
@@ -93,6 +96,8 @@ function MyTookPage() {
   });
 
   const groupedMembers = groupByMonth(filteredMembers, 'createdAt');
+
+  const sendMoneyData = groupedMembers;
 
   return (
     <div className="flex flex-col bg-white max-w-[360px] mx-auto relative h-screen font-[Nanum Gothic]">
@@ -131,11 +136,15 @@ function MyTookPage() {
             </div>
           ))
         ) : (
-          Object.keys(groupedMembers).map(month => (
+          Object.keys(sendMoneyData).map(month => (
             <div key={month}>
               <div className="text-sm font-bold text-gray-700 mb-4">{month}</div>
-              {groupedMembers[month].map(member => (
-                <SendMoneyCard key={member.member_seq} {...member} />
+              {sendMoneyData[month].map(member => (
+                <SendMoneyCard 
+                  key={member.member_seq} 
+                  {...member} 
+                  status={member.status}
+                />
               ))}
             </div>
           ))
@@ -146,3 +155,4 @@ function MyTookPage() {
 }
 
 export default MyTookPage;
+
