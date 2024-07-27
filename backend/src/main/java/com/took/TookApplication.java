adeed5bc565e7cac37af681edd454b0ab0131d30
rinch12332 <rinch12332@gmail.com>
2024-07-27 16:52:33 +0900

    feat: SMS-api

--- backend/src/main/java/com/took/TookApplication.java
+++ backend/src/main/java/com/took/TookApplication.java
@@ -2,8 +2,10 @@ package com.took;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories // Redis 리포지토리 활성화
 public class TookApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/took/config/RedisConfig.java
+++ backend/src/main/java/com/took/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.positionsave_api.config;
+package com.took.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
