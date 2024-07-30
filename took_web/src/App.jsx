936382e55ff2520d573b8ca3cf2472bec529f9d0
차민주 <ckalswn007@naver.com>
2024-07-31 03:07:45 +0900

    feat: 택시 took 채팅방 구현 중

--- took_web/src/App.jsx
+++ took_web/src/App.jsx
@@ -57,6 +57,7 @@ import CurrentPathListPage from './pages/taxi/CurrentPathListPage';
 import DeliveryChattingMainPage from './pages/chat/DeliveryChattingMainPage';
 import ChattingListPage from './pages/chat/ChattingListPage';
 import TookChattingPage from './pages/chat/TookChattingPage';
+import TaxiChattingMainPage from './pages/chat/TaxiChattingMainPage';
 
 function App() {
   const [checkedItems, setCheckedItems] = useState({
@@ -132,6 +133,7 @@ function App() {
     { path: '/taxi/path', element: <TaxiPathSettingPage /> },
     { path: '/taxi/path-list', element: <CurrentPathListPage /> },
     { path: '/chat/delivery/main', element: <DeliveryChattingMainPage /> },
+    { path: '/chat/taxi/main', element: <TaxiChattingMainPage /> },
     { path: '/chat/list', element: <ChattingListPage /> },
     { path: '/chat/took', element: <TookChattingPage /> },
     { path: '/groupbuy/my-purchase', element: <MyPurchasePage /> },
