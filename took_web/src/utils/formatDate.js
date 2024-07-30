94ee6ae9dc5c1d472cdb5b8f18ac81cc03d625e0
조현정 <whguswjd456@naver.com>
2024-07-30 17:41:52 +0900

    refacor : 다른 날짜 형식 함수 추가

--- took_web/src/utils/formatDate.js
+++ took_web/src/utils/formatDate.js
@@ -16,3 +16,38 @@ export const formatDateWithoutTime = (dateString) => {
   const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'][date.getDay()];
   return `${month}.${day} (${dayOfWeek})`;
 };
+
+
+export const formatDateWithYear = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const weekday = ['일', '월', '화', '수', '목', '금', '토'][date.getDay()];
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${year}.${month}.${day} (${weekday}) ${formattedHours}:${formattedMinutes}`;
+}
+
+export const formatDateOnly = (dateString) => {
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    weekday: 'long',
+  };
+  const date = new Date(dateString);
+  return date.toLocaleDateString('ko-KR', options);
+}
+
+export const formatTime = (dateString) => {
+  const date = new Date(dateString);
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const ampm = hours >= 12 ? '오후' : '오전';
+  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return `${ampm} ${formattedHours}:${formattedMinutes}`;
+}
