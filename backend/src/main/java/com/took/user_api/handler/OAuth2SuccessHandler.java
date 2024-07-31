795b5b2b2393de805bfaa1ae216e5187ac1c7440
이재찬 <jaechanjj@naver.com>
2024-07-31 17:37:15 +0900

    feat: kakao 로그인 조정

--- backend/src/main/java/com/took/user_api/handler/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/took/user_api/handler/OAuth2SuccessHandler.java
@@ -16,27 +16,31 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-
     private final JwtProvider jwtProvider;
 
     @Override
     public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) throws IOException, ServletException {
 
-        HttpServletRequest request, 
-        HttpServletResponse response,
-        Authentication authentication
-        
-        ) throws IOException, ServletException {
-            
-            CustomOAuth2User oAuth2User =(CustomOAuth2User)authentication.getPrincipal();
+        CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-            String userId = oAuth2User.getName();
-            
-            String accessToken = jwtProvider.createAccessToken(userId);
-            String resfreshToken = jwtProvider.createRefreshToken(userId);
+        String userId = oAuth2User.getName();
 
-            response.sendRedirect("http://localhost:3000/auth/oauth-response/"+"token:"+accessToken+"/3600");
-        }
+        String accessToken = jwtProvider.createAccessToken(userId);
+        String refreshToken = jwtProvider.createRefreshToken(userId);
 
+        // HTML과 JavaScript를 통해 로컬 스토리지와 쿠키에 토큰 저장
+        String redirectUrl = "https://i11e205.p.ssafy.io/";
+        String script = "<script>" +
+                "localStorage.setItem('accessToken', '" + accessToken + "');" +
+                "document.cookie = 'refreshToken=" + refreshToken + "; path=/;';" +
+                "window.location.href = '" + redirectUrl + "';" +
+                "</script>";
 
+        response.setContentType("text/html");
+        response.getWriter().write(script);
+    }
 }
