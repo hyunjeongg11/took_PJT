7b671d12f82fda7f374c2a7ca5e063f06bf546e6
조현정 <guswjd4585@gmail.com>
2024-08-04 15:06:00 +0900

    refactor : App.jsx에서 사용한 변수들 제거, agreement, agreementdetail에서 각자 관리하도록 수정

--- took_web/src/App.jsx
+++ took_web/src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+// src/pages/App.jsx
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import {
@@ -61,91 +62,66 @@ import TookChattingPage from './pages/chat/TookChattingPage';
 import TaxiChattingMainPage from './pages/chat/TaxiChattingMainPage';
 import GroupBuyChattingMainPage from './pages/chat/GroupBuyChattingMainPage';
 
+const ROUTER = createBrowserRouter([
+  { path: '/', element: <MainPage /> },
+  { path: '/login', element: <LoginPage /> },
+  { path: '/signup', element: <SignupPage /> },
+  { path: '/userinfo', element: <UserInfoPage /> },
+  { path: '/payment', element: <PaymentPage /> },
+  { path: '/modifyPwd', element: <ModifyPwdPage /> },
+  { path: '/pwd', element: <PwdPage /> },
+  { path: '/account', element: <AccountPage /> },
+  { path: '/select', element: <SelectPage /> },
+  { path: '/agreement', element: <AgreementPage /> },
+  { path: '/agreementdetail', element: <AgreementDetailPage /> },
+  { path: '/verification', element: <VerificationPage /> },
+  { path: '/accountcomplete', element: <AccountCompletePage /> },
+  { path: '/setsimplepwd', element: <SetSimplePwd /> },
+  { path: '/mypage', element: <MyPage /> },
+  { path: '/notification', element: <NotificationSetting /> },
+  { path: '/location', element: <LocationSettingPage /> },
+  { path: '/tookDetails', element: <TookDetailsPage /> },
+  { path: '/tookHistory', element: <TookHistoryPage /> },
+  { path: '/complete', element: <CompletePage /> },
+  { path: '/payment-methods', element: <PaymentMethods /> },
+  { path: '/transaction-history', element: <TransactionHistoryPage /> },
+  { path: '/transaction-detail', element: <TransactionDetailPage /> },
+  { path: '/mytook', element: <MyTookPage /> },
+  { path: '/sendinput', element: <SendInputPage /> },
+  { path: '/dutch/userlist', element: <UserListPage /> },
+  { path: '/dutch/input', element: <PaymentInputPage /> },
+  { path: '/dutch/total', element: <PaymentTotalPage /> },
+  { path: '/dutch/request', element: <PayRequestPage /> },
+  { path: '/mytookmoney', element: <MyTookMoneyPage /> },
+  { path: '/delivery/create', element: <CreateDeliveryPage /> },
+  { path: '/delivery/detail', element: <DeliveryDetailPage /> },
+  { path: '/delivery/list', element: <DeliveryListPage /> },
+  { path: '/delivery/input', element: <DeliveryPayInputPage /> },
+  { path: '/delivery/complete', element: <DeliveryCompletePage /> },
+  { path: '/delivery/status', element: <DeliveryStatusPage /> },
+  { path: '/groupbuy/list', element: <BuyListPage /> },
+  { path: '/groupbuy/:id', element: <BuyDetailPage /> },
+  { path: '/groupbuy/join/:id', element: <BuyJoinPage /> },
+  { path: '/groupbuy/form', element: <BuyFormPage /> },
+  { path: '/taxi/input', element: <TaxiCostInputPage /> },
+  { path: '/taxi/request', element: <TaxiCostRequestPages /> },
+  { path: '/taxi/main', element: <TaxiMainPage /> },
+  { path: '/taxi/create', element: <CreateChattingPage /> },
+  { path: '/taxi/setting', element: <TaxiChattingSettingPage /> },
+  { path: '/taxi/path', element: <TaxiPathSettingPage /> },
+  { path: '/taxi/path-list', element: <CurrentPathListPage /> },
+  { path: '/chat/delivery/main', element: <DeliveryChattingMainPage /> },
+  { path: '/chat/groupbuy/main', element: <GroupBuyChattingMainPage /> },
+  { path: '/chat/taxi/main', element: <TaxiChattingMainPage /> },
+  { path: '/chat/list', element: <ChattingListPage /> },
+  { path: '/chat/took', element: <TookChattingPage /> },
+  { path: '/groupbuy/my-purchase', element: <MyPurchasePage /> },
+  { path: '/groupbuy/order', element: <OrderFormPage /> },
+  { path: '/groupbuy/total/:id', element: <TotalPurchasePage /> },
+  { path: '/groupbuy/my-order', element: <MyOrderFormPage /> },
+]);
+
 function App() {
-  const [checkedItems, setCheckedItems] = useState({
-    terms1: false,
-    terms2: false,
-    terms3: false,
-    terms4: false,
-    terms5: false,
-    terms6: false,
-  });
-
-  const ROUTER = createBrowserRouter([
-    { path: '/', element: <MainPage /> },
-    { path: '/login', element: <LoginPage /> },
-    { path: '/signup', element: <SignupPage /> },
-    { path: '/userinfo', element: <UserInfoPage /> },
-    { path: '/payment', element: <PaymentPage /> },
-    { path: '/modifyPwd', element: <ModifyPwdPage /> },
-    { path: '/pwd', element: <PwdPage /> },
-    { path: '/account', element: <AccountPage /> },
-    { path: '/select', element: <SelectPage /> },
-    {
-      path: '/agreement',
-      element: (
-        <AgreementPage
-          checkedItems={checkedItems}
-          setCheckedItems={setCheckedItems}
-        />
-      ),
-    },
-    {
-      path: '/agreementdetail',
-      element: (
-        <AgreementDetailPage
-          checkedItems={checkedItems}
-          setCheckedItems={setCheckedItems}
-        />
-      ),
-    },
-    { path: '/verification', element: <VerificationPage /> },
-    { path: '/accountcomplete', element: <AccountCompletePage /> },
-    { path: '/setsimplepwd', element: <SetSimplePwd /> },
-    { path: '/mypage', element: <MyPage /> },
-    { path: '/notification', element: <NotificationSetting /> },
-    { path: '/location', element: <LocationSettingPage /> },
-    { path: '/tookDetails', element: <TookDetailsPage /> },
-    { path: '/tookHistory', element: <TookHistoryPage /> },
-    { path: '/complete', element: <CompletePage /> },
-    { path: '/payment-methods', element: <PaymentMethods /> },
-    { path: '/transaction-history', element: <TransactionHistoryPage /> },
-    { path: '/transaction-detail', element: <TransactionDetailPage /> },
-    { path: '/mytook', element: <MyTookPage /> },
-    { path: '/sendinput', element: <SendInputPage /> },
-    { path: '/dutch/userlist', element: <UserListPage /> },
-    { path: '/dutch/input', element: <PaymentInputPage /> },
-    { path: '/dutch/total', element: <PaymentTotalPage /> },
-    { path: '/dutch/request', element: <PayRequestPage /> },
-    { path: '/mytookmoney', element: <MyTookMoneyPage /> },
-    { path: '/delivery/create', element: <CreateDeliveryPage /> },
-    { path: '/delivery/detail', element: <DeliveryDetailPage /> },
-    { path: '/delivery/list', element: <DeliveryListPage /> },
-    { path: '/delivery/input', element: <DeliveryPayInputPage /> },
-    { path: '/delivery/complete', element: <DeliveryCompletePage /> },
-    { path: '/delivery/status', element: <DeliveryStatusPage /> },
-    { path: '/groupbuy/list', element: <BuyListPage /> },
-    { path: '/groupbuy/:id', element: <BuyDetailPage /> },
-    { path: '/groupbuy/join/:id', element: <BuyJoinPage /> },
-    { path: '/groupbuy/form', element: <BuyFormPage /> },
-    { path: '/taxi/input', element: <TaxiCostInputPage /> },
-    { path: '/taxi/request', element: <TaxiCostRequestPages /> },
-    { path: '/taxi/main', element: <TaxiMainPage /> },
-    { path: '/taxi/create', element: <CreateChattingPage /> },
-    { path: '/taxi/setting', element: <TaxiChattingSettingPage /> },
-    { path: '/taxi/path', element: <TaxiPathSettingPage /> },
-    { path: '/taxi/path-list', element: <CurrentPathListPage /> },
-    { path: '/chat/delivery/main', element: <DeliveryChattingMainPage /> },
-    { path: '/chat/groupbuy/main', element: <GroupBuyChattingMainPage /> },
-    { path: '/chat/taxi/main', element: <TaxiChattingMainPage /> },
-    { path: '/chat/list', element: <ChattingListPage /> },
-    { path: '/chat/took', element: <TookChattingPage /> },
-    { path: '/groupbuy/my-purchase', element: <MyPurchasePage /> },
-    { path: '/groupbuy/order', element: <OrderFormPage /> },
-    { path: '/groupbuy/total/:id', element: <TotalPurchasePage /> },
-    { path: '/groupbuy/my-order', element: <MyOrderFormPage /> },
-  ]);
-
   return <RouterProvider router={ROUTER} />;
 }
 
