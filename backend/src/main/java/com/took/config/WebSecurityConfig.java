501c4e87a7f10d9fbc5f08dbb3f1902ae98bdbce
rinch12332 <rinch12332@gmail.com>
2024-08-05 09:49:02 +0900

    feat: swagger 권한 수정

--- backend/src/main/java/com/took/config/WebSecurityConfig.java
+++ backend/src/main/java/com/took/config/WebSecurityConfig.java
@@ -78,8 +78,9 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(request -> request
 
 //                                                로그인 전에 이뤄지는 작업은 인증이 푤요하지 않다.
-                                .requestMatchers("/", "/api/auth/**","/oauth2/**", "/api/swagger-ui/**").permitAll()
+                                .requestMatchers("/", "/api/auth/**","/oauth2/**").permitAll()
                                 .requestMatchers("/ws/**").permitAll()
+                                .requestMatchers( "/","/swagger-ui/**", "/api-docs/**").permitAll()
                                 // 접두사는 제외 하고 USER만 적어준다.w
                                 .requestMatchers("/api/user/**").hasRole("USER")
                                 .requestMatchers("/api/account/**").hasRole("USER")
