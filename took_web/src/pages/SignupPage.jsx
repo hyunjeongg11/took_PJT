913666a62cbedf7b5660bf797448d4855b42e0d1
정희수 <s007kk@ajou.ac.kr>
2024-08-06 21:32:33 +0900

    feat: 메인 화면 구현 및 페이지 라우팅 연결

--- took_web/src/pages/SignupPage.jsx
+++ took_web/src/pages/SignupPage.jsx
@@ -215,6 +215,7 @@ function SignupPage() {
             value={id}
             onChange={(e) => {
               setId(e.target.value);
+              setIdError('');
               setIsIdValid(false);
             }}
             placeholder="아이디를 입력해주세요"
