de44eb1fc40644a04ada04a94fa20133a6ffe5d6
이재찬 <jaechanjj@naver.com>
2024-08-07 09:31:21 +0900

    fix: redirect 문제 해결

--- backend/src/main/java/com/took/config/WebSecurityConfig.java
+++ backend/src/main/java/com/took/config/WebSecurityConfig.java
@@ -57,6 +57,9 @@ public class WebSecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         .maximumSessions(1)
                         .sessionRegistry(sessionRegistry()))
+                .authorizeHttpRequests(authorize ->authorize
+                        .requestMatchers("/","/login","/signup").permitAll()
+                )
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/", "/api/auth/**", "/api/oauth2/**").permitAll()
                         .requestMatchers("/ws/**").permitAll()
