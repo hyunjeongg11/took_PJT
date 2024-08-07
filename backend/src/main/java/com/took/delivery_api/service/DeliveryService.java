10a561b19d8cbaedfde27d4f616d44568dc700ff
rinch12332 <rinch12332@gmail.com>
2024-08-07 15:07:01 +0900

    feat:

--- backend/src/main/java/com/took/delivery_api/service/DeliveryService.java
+++ backend/src/main/java/com/took/delivery_api/service/DeliveryService.java
@@ -99,7 +99,7 @@ public class DeliveryService {
     @Transactional
     public List<DeliverySelectResponse> getList(DeliveryListSelectRequest request) {
         List<Delivery> deliveryList = deliveryRepository.findAll();
-
+        System.out.println("request: " + request.getLat() + ", " + request.getLon());
         return deliveryList.stream()
                 .map(delivery -> {
                     double deliveryLat = delivery.getPickupLat();
