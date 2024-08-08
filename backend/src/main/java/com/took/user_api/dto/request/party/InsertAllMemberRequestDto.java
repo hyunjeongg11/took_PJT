30ccdd2598299bb09812b0cef28076201bf21d15
rinch12332 <rinch12332@gmail.com>
2024-08-08 21:25:13 +0900

    feat: 정산 API Swagger 수정

--- backend/src/main/java/com/took/user_api/dto/request/party/InsertAllMemberRequestDto.java
+++ backend/src/main/java/com/took/user_api/dto/request/party/InsertAllMemberRequestDto.java
@@ -16,7 +16,7 @@ public class InsertAllMemberRequestDto {
     @Schema(description = "유저별 비용 목록", required = true)
     private List<userCost> userCosts;
 
-    @Schema(description = "배달 팁", example = "5000")
+    @Schema(description = "배달 팁 (배달, 공구에서만)", example = "5000")
     private Long deliveryTip;
 
     @Getter
