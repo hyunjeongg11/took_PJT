14e3a99cd4551763e5f6c9241cb3ac331eae089b
김태훈 <zxader@naver.com>
2024-08-15 23:41:17 +0900

    dev-fe

--- backend/src/main/java/com/took/sms_api/repository/IdentityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.took.sms_api.repository;
-
-import com.took.sms_api.entity.Identity;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface IdentityRepository extends CrudRepository<Identity, String> {
-}
--- backend/src/main/java/com/took/user_api/service/TokenBlacklistService.java
+++
@@ -1,9 +0,0 @@
-package com.took.user_api.service;
-
-import java.util.concurrent.TimeUnit;
-
-public interface TokenBlacklistService {
-    void addToken(String Token,long duration, TimeUnit unit);
-    boolean isTokenBlacklisted(String token);
-    long getExpiryFromToken(String Token);
-}
